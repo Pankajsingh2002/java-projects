7902c9b37900d528cb3ae406a77430e6eb24b2b3
Pankaj singh <singhpankaj09082002@gmail.com>
2024-12-29 12:40:12 +0530

    Add files via upload

--- basicsofloops.java
+++ basicsofloops.java
@@ -2,7 +2,8 @@ package loops;
 
 public class basicsofloops {
     public static void main(String[] args) {
-        for(int i=1;i<=5;i++){
+
+        for(int i=1;i<=12;i++){
             System.out.println("hello world");
         }
     }
